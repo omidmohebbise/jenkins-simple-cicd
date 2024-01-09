555cccb9e00f24a87dc3520523b8a25b7dbf123c
omid mohebbi <omidmohebbise@gmail.com>
2024-01-09 10:29:01 +0100

    initial the project

---
+++ src/main/java/org/omidmohebbise/jenkins/jenkinssimplecdcd/JenkinsSimpleCdcdApplication.java
@@ -0,0 +1,13 @@
+package org.omidmohebbise.jenkins.jenkinssimplecdcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSimpleCdcdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSimpleCdcdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/omidmohebbise/jenkins/jenkinssimplecdcd/JenkinsSimpleCdcdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omidmohebbise.jenkins.jenkinssimplecdcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSimpleCdcdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
